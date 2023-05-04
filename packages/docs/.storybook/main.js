7e8d1058222f95dfbcb544a8f28146d79b901f38
pdavinunes <p.davi.nunes@gmail.com>
2023-05-03 22:18:21 -0300

    feat: add all components

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,18 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
   },
   "features": {
     "storyStoreV7": true
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
